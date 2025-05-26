941a86cac8e519cc5c97f7ab7b4c1c36bbef5f89
Niccar95 <niccar.95@outlook.com>
2025-05-26 19:16:45 +0200

    added spinner

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,24 @@ import { router } from "./Router";
 import "./styles/App.scss";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
+import { useEffect, useState } from "react";
+import Spinner from "./components/Spinner";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
   return (
     <>
       <I18nextProvider i18n={i18n}>
---
+++ src/components/Spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="spinnerContainer">
+      <img src="/Ellipsis@1x-1.0s-200px-200px.svg" alt="Loading..." />
+    </div>
+  );
+};
+
+export default Spinner;
