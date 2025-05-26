2696a0812182c29cc7d0980eb5ca2f44c8d43938
Niccar95 <niccar.95@outlook.com>
2025-05-26 14:52:38 +0200

    styled 404 page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,8 +1,29 @@
+import { motion } from "motion/react";
+import { useNavigate } from "react-router-dom";
 const Error = () => {
+  const navigate = useNavigate();
+
+  const returnHome = () => {
+    navigate("/");
+  };
+
   return (
-    <>
-      <h1>404 Page not found!</h1>
-    </>
+    <motion.div
+      className="notFoundContainer"
+      initial={{ opacity: 0, scale: 0.6 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.8, ease: "easeOut" }}
+    >
+      <h1 className="notFound">404 Page not found!</h1>
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        transition={{ duration: 0.2 }}
+        onClick={returnHome}
+        className="returnButton"
+      >
+        Back to Home Page
+      </motion.button>
+    </motion.div>
   );
 };
 
